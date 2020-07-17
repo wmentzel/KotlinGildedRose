a6e96babad903157375e3ec5b03bcff69f4617dc
Willi Mentzel <willi.mentzel@gmail.com>
2020-07-17 13:09:11 +0200

    reduce dexterity vest

--- src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
@@ -45,26 +45,11 @@ class Shop(val items: List<Item>) {
         }
         ItemNames.DexterityVest -> {
           if (item.quality > 0) {
-            if (item.name != "Sulfuras, Hand of Ragnaros" && !item.name.contains("conjured", ignoreCase = true)) {
-              --item.quality
-            }
-
-            if (item.name.contains("conjured", ignoreCase = true)) {
-              item.quality -= 2
-            }
+            --item.quality
           }
-          --item.sellIn
 
-          if (item.sellIn < 0) {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros" && !item.name.contains("conjured", ignoreCase = true)) {
-                --item.quality
-              }
-
-              if (item.name.contains("conjured", ignoreCase = true)) {
-                item.quality -= 2
-              }
-            }
+          if (--item.sellIn < 0 && item.quality > 0) {
+            --item.quality
           }
         }
         ItemNames.ElixirOfTheMongoose -> {
