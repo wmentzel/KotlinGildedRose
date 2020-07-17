e99e0b718c92c99e85adc9a21b2180832fa12928
Willi Mentzel <willi.mentzel@gmail.com>
2020-07-17 13:04:07 +0200

    reduce aged brie

--- src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
@@ -15,15 +15,13 @@ class Shop(val items: List<Item>) {
 
       when (ItemNames.values().find { it.itemName == item.name }) {
         ItemNames.AgedBrie -> {
+
           if (item.quality < 50) {
             ++item.quality
           }
-          --item.sellIn
 
-          if (item.sellIn < 0) {
-            if (item.quality < 50) {
-              ++item.quality
-            }
+          if (--item.sellIn < 0 && item.quality < 50) {
+            ++item.quality
           }
         }
         ItemNames.BackstagePasses -> {
