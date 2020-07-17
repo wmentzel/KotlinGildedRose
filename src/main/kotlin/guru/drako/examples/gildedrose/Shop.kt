2edee6c8701efbc15e3c7e370fff4833bdfb1eda
Willi Mentzel <willi.mentzel@gmail.com>
2020-07-17 13:04:36 +0200

    reduce backstage passes

--- src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
@@ -29,21 +29,15 @@ class Shop(val items: List<Item>) {
             ++item.quality
           }
 
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              ++item.quality
-            }
+          if (item.sellIn < 11 && item.quality < 50) {
+            ++item.quality
           }
 
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              ++item.quality
-            }
+          if (item.sellIn < 6 && item.quality < 50) {
+            ++item.quality
           }
 
-          --item.sellIn
-
-          if (item.sellIn < 0) {
+          if (--item.sellIn < 0) {
             item.quality -= item.quality
           }
         }
