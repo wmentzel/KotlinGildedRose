6ffe13e7d229b750af4a213884eba30bdacf1f60
Willi Mentzel <willi.mentzel@gmail.com>
2020-07-19 09:58:01 +0200

    use enum class after all, to make when exhaustive

--- src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
@@ -4,6 +4,23 @@ const val AGE_BRIE = "Aged Brie"
 const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
 const val SULFURAS = "Sulfuras, Hand of Ragnaros"
 
+enum class ItemType {
+  AgedBrie,
+  BackstagePasses,
+  ConjuredItems,
+  UsualItems,
+  Sulfuras
+}
+
+val Item.type
+  get() = when {
+    this.name == AGE_BRIE -> ItemType.AgedBrie
+    this.name.startsWith("backstage passes", ignoreCase = true) -> ItemType.BackstagePasses
+    this.name == SULFURAS -> ItemType.Sulfuras
+    this.name.startsWith("conjured", ignoreCase = true) -> ItemType.ConjuredItems
+    else -> ItemType.UsualItems
+  }
+
 class Shop(val items: List<Item>) {
   fun runForOneDay() {
     items.forEach(this::updateItem)
@@ -11,23 +28,22 @@ class Shop(val items: List<Item>) {
 
   private fun updateItem(item: Item) {
 
-    if (item.name == SULFURAS) {
+    if (item.type == ItemType.Sulfuras) {
       return
     }
 
     --item.sellIn
 
-    if (item.name in listOf(AGE_BRIE, BACKSTAGE_PASSES)) {
-      increaseQualityWithinBounds(item)
-    }
-
-    when (item.name) {
-      AGE_BRIE -> {
+    when (item.type) {
+      ItemType.AgedBrie -> {
+        increaseQualityWithinBounds(item)
         if (item.sellIn < 0) {
           increaseQualityWithinBounds(item)
         }
       }
-      BACKSTAGE_PASSES -> {
+      ItemType.BackstagePasses -> {
+
+        increaseQualityWithinBounds(item)
 
         listOf(10, 5).forEach {
           if (item.sellIn + 1 <= it) {
@@ -39,18 +55,28 @@ class Shop(val items: List<Item>) {
           item.quality = 0
         }
       }
-      else -> {
+      ItemType.ConjuredItems -> {
+        decreaseQualityWithinBounds(item, 2)
 
-        val degradeBy = if (item.name.contains("conjured", ignoreCase = true)) 2 else 1
-
-        if (item.quality > 0) {
-          item.quality -= degradeBy
-        }
-
-        if (item.sellIn < 0 && item.quality > 0) {
-          item.quality -= degradeBy
+        if (item.sellIn < 0) {
+          decreaseQualityWithinBounds(item, 2)
         }
       }
+      ItemType.UsualItems -> {
+        decreaseQualityWithinBounds(item, 1)
+
+        if (item.sellIn < 0) {
+          decreaseQualityWithinBounds(item, 1)
+        }
+      }
+      ItemType.Sulfuras -> {
+      }
+    }
+  }
+
+  private fun decreaseQualityWithinBounds(item: Item, degradeBy: Int) {
+    if (item.quality > 0) {
+      item.quality -= degradeBy
     }
   }
 
