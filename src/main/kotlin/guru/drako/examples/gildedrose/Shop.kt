371bbdf430a6b253a9908354158a78fec97ac145
Willi Mentzel <willi.mentzel@gmail.com>
2020-07-19 10:11:18 +0200

    reduce logic

--- src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
@@ -1,5 +1,8 @@
 package guru.drako.examples.gildedrose
 
+import kotlin.math.max
+import kotlin.math.min
+
 const val AGE_BRIE = "Aged Brie"
 const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
 const val SULFURAS = "Sulfuras, Hand of Ragnaros"
@@ -35,54 +38,34 @@ class Shop(val items: List<Item>) {
     --item.sellIn
 
     when (item.type) {
-      ItemType.AgedBrie -> {
-        increaseQualityWithinBounds(item)
-        if (item.sellIn < 0) {
-          increaseQualityWithinBounds(item)
-        }
-      }
+      ItemType.AgedBrie -> increaseQualityWithinBounds(item, if (item.sellIn < 0) 2 else 1)
+
       ItemType.BackstagePasses -> {
 
-        increaseQualityWithinBounds(item)
-
-        listOf(10, 5).forEach {
-          if (item.sellIn + 1 <= it) {
-            increaseQualityWithinBounds(item)
-          }
+        val increaseBy = when {
+          item.sellIn + 1 <= 5 -> 3
+          item.sellIn + 1 <= 10 -> 2
+          else -> 1
         }
+
+        increaseQualityWithinBounds(item, increaseBy)
 
         if (item.sellIn < 0) {
           item.quality = 0
         }
       }
-      ItemType.ConjuredItems -> {
-        decreaseQualityWithinBounds(item, 2)
-
-        if (item.sellIn < 0) {
-          decreaseQualityWithinBounds(item, 2)
-        }
-      }
-      ItemType.UsualItems -> {
-        decreaseQualityWithinBounds(item, 1)
-
-        if (item.sellIn < 0) {
-          decreaseQualityWithinBounds(item, 1)
-        }
-      }
+      ItemType.ConjuredItems -> decreaseQualityWithinBounds(item, if (item.sellIn < 0) 4 else 2)
+      ItemType.UsualItems -> decreaseQualityWithinBounds(item, if (item.sellIn < 0) 2 else 1)
       ItemType.Sulfuras -> {
       }
     }
   }
 
   private fun decreaseQualityWithinBounds(item: Item, degradeBy: Int) {
-    if (item.quality > 0) {
-      item.quality -= degradeBy
-    }
+    item.quality = max(item.quality - degradeBy, 0)
   }
 
-  private fun increaseQualityWithinBounds(item: Item) {
-    if (item.quality < 50) {
-      ++item.quality
-    }
+  private fun increaseQualityWithinBounds(item: Item, increaseBy: Int) {
+    item.quality = min(item.quality + increaseBy, 50)
   }
 }
